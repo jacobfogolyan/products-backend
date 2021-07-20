4a96c7e38f9c012a052b151502cdc15d11cb4b41
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2021-07-20 14:12:19 +1000

    added product model tsconfig path and response status for create Product

--- routes/productsRoute.ts
+++ routes/productsRoute.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from 'express'
+import { createProduct } from '@model/products'
 
 const app = express()
 
 app.post('/create', (req: Request, res: Response) => {
-    // createProduct
-    throw new Error('Method Not implemented')
+    const { name, size, material, color } = req.body
+    createProduct(name, size, material, color).then(product => {
+        res.status(200).send(product)
+    }).catch((err) => {
+        res.status(500).send(err)
+    })
 })
 
 app.get('/read', (req: Request, res: Response) => {
