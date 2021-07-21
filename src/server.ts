bb7121ae06e4c2c6d6bdb7be6579d87753b5f5e9
Jacob Fogolyan <jacob.fogolyan@gmail.com>
2021-07-21 13:45:42 +1000

    added allowed methods to prevent cors errors

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
     next();
 })
 
